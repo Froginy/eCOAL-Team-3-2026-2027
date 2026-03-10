737dd3f15a4a2d635e7e43c82de7c9be9415a391
Poteau Laly <lalypoteau1@gmail.com>
2026-03-10 14:15:34 +0100

    added svg dices - base html & scss -> feed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,12 +1,15 @@
 import logo from '../../assets/logo.svg'
 import './feed.css'
 import PostCard from '../postCards/postCards.jsx'
-
+import sort from '../../assets/sort.svg'
 function Feed()   {
 
     return(
         <div className="feed">
+            <div className="feed-header">
             <img src={logo} alt="Logo" />
+            <a href=""><img src={sort} alt="Sort" /></a>
+            </div>
             <PostCard />
             <PostCard />
             <PostCard />
--- client/src/components/postCards/postCards.jsx
+++ client/src/components/postCards/postCards.jsx
@@ -1,6 +1,11 @@
 import './postCard.css'
 import placeholder_dice from '../../assets/placeholder_dice.png'
 import PostBar from '../PostBar/PostBar.jsx'
+import arrow from '../../assets/arrow.svg'
+import dots from '../../assets/dots.svg'
+import dice1 from '../../assets/dice1.svg'
+import dice2 from '../../assets/dice2.svg'
+import dice3 from '../../assets/dice3.svg'
 
 function PostCard()   {
 
@@ -14,8 +19,17 @@ function PostCard()   {
         
         <PostBar />
         <img src={placeholder_dice} alt="dice_picture" className='DicePic'/>
-        
-        <p>Description</p>
+            <a href="" className="next"><img src={arrow} alt="arrow" /></a>
+            <div className='dicesNumbers'>
+            <img src={dice1} alt="dice1" />
+            <img src={dice2} alt="dice2" />
+            <img src={dice3} alt="dice3" />
+            </div>
+
+            <a href="" className="more"> 
+                <img src={dots} alt="dots" />
+            </a>
+        <p>Cupidatat aute laborum aliqua consectetur voluptate laborum ipsum pariatur est deserunt enim eiusmod adipisicing duis. </p>
         
         </div>)
 }
