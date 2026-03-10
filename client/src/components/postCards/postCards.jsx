87570ec7c0bf6c50e97b620947a31beb2eb74897
Poteau Laly <lalypoteau1@gmail.com>
2026-03-10 16:54:22 +0100

    added some variety for laptop version and some fixes

--- client/src/components/postCards/postCards.jsx
+++ client/src/components/postCards/postCards.jsx
@@ -12,22 +12,23 @@ const postContent = "Cupidatat aute laborum aliqua consectetur voluptate laborum
 
     return(
     
-        <div className='relative w-[300px] mb-7.5'>
+        <div className='relative w-75 mb-7.5 bg-white shadow-lg rounded-2xl flex flex-col '>
         
         <PostBar />
-        <img src={placeholder_dice} alt="dice_picture" className='aspect-square object-cover w-[300px] text-center rounded-3xl shadow-lg'/>
-            <a href="" className="absolute top-[140px] aspect-square w-5 -right-3.5 flex justify-center items-center bg-white rounded-full p-1.25"><img src={arrow} alt="arrow" className="rotate-[-90deg] h-1.5" /></a>
-            <div className='flex justify-center absolute top-[265px] gap-1.25 ml-1.25'>
-            <img src={dice1} alt="dice1" className="w-5 odd:rotate-[15deg] even:rotate-[-15deg]" />
-            <img src={dice2} alt="dice2" className="w-5 odd:rotate-[15deg] even:rotate-[-15deg]" />
-            <img src={dice3} alt="dice3" className="w-5 odd:rotate-[15deg] even:rotate-[-15deg]" />
+        <img src={placeholder_dice} alt="dice_picture" className='aspect-square object-cover w-[110%] self-center rounded-3xl shadow-lg' />
+            <a href="" className="absolute top-35 aspect-square w-5 -right-3.5 flex justify-center items-center bg-white rounded-full p-1.25"><img src={arrow} alt="arrow" className="rotate-270 h-1.5" /></a>
+            <div className='flex justify-center absolute top-66.25 md:top-90 gap-1.25 ml-1.25'>
+            <img src={dice1} alt="dice1" className="w-5 odd:rotate-15 even:rotate--15" />
+            <img src={dice2} alt="dice2" className="w-5 odd:rotate-15 even:rotate--15" />
+            <img src={dice3} alt="dice3" className="w-5 odd:rotate-15 even:rotate--15" />
             </div>
 
-            <a href="" className="absolute aspect-square w-5 right-5 top-[265px] flex justify-center items-center"> 
-                <img src={dots} alt="dots" className="h-[26px]" />
+            <a href="" className="absolute aspect-square w-5 right-5 top-66.25 md:top-90 flex justify-center items-center"> 
+                <img src={dots} alt="dots" className="h-6.5" />
             </a>
-        <p className="text-left leading-5">{postContent}</p>
-        
+            <div className='flex flex-col '>
+        <p className="text-left leading-5 w-75 p-4">{postContent}</p>
+        </div>
         </div>)
 }
 
