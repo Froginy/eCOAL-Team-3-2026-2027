d4e8fe7404848e71f29fea2e3b5b0e0d4b4296b2
vayd0 <kiiroxyz@gmail.com>
2026-03-12 12:50:50 +0100

    Settings

--- client/src/components/PostBar/PostBar.jsx
+++ client/src/components/PostBar/PostBar.jsx
@@ -1,52 +1,62 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import add from '../../assets/add.svg'
-import like from '../../assets/like.svg'
-import placeholder from '../../assets/placeholder_pp.jpg'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import add from "../../assets/add.svg";
+import like from "../../assets/like.svg";
+import placeholder from "../../assets/placeholder_pp.jpg";
+import { Link } from "react-router-dom";
 
 function PostBar({ user_id }) {
-    const serverURL = 'http://127.0.0.1:8000/'; 
-    const [user, setUser] = useState(); 
+  const serverURL = import.meta.env.VITE_API_URL;
+  const [user, setUser] = useState();
 
-    useEffect(() => {
-        const getProtectedUser = async () => {
-            if (!user_id) return; 
-            try {
-                const response = await axios.get(`${serverURL}api/users/${user_id}`);
-                
-                setUser(response.data.data); 
-            } catch (error) {
-                console.error("API Error:", error.response?.status);
+  useEffect(() => {
+    const getProtectedUser = async () => {
+      if (!user_id) return;
+      try {
+        const response = await axios.get(`${serverURL}/users/${user_id}`);
+
+        setUser(response.data.data);
+      } catch (error) {
+        console.error("API Error:", error.response?.status);
+      }
+    };
+
+    getProtectedUser();
+  }, [user_id]);
+
+  return (
+    <div className="flex flex-row justify-between items-center bg-white text-black w-56.25 md:w-75 h-10 mx-auto rounded-3xl absolute left-2.5 top-2.5 z-50 shadow-sm">
+      <div className="flex items-center relative text-black m-2.5">
+        <Link
+          to={`/profile/${user?.id}`}
+          className="flex items-center relative gap-2 text-black "
+        >
+          <img
+            src={
+              user?.profile_image
+                ? `${serverURL}${user.profile_image}`
+                : placeholder
             }
-        };
+            alt="profile_picture"
+            className="rounded-full w-7 aspect-square object-cover"
+          />
 
-        getProtectedUser(); 
-    }, [user_id]); 
+          <p className="text-black w-20 overflow-wrap-anywhere whitespace-normal text-xs font-bold leading-tight">
+            {user ? user.name : "Chargement..."}
+          </p>
+        </Link>
+      </div>
 
-    return (
-        <div className='flex flex-row justify-between items-center bg-white text-black w-56.25 md:w-75 h-10 mx-auto rounded-3xl absolute left-2.5 top-2.5 z-50 shadow-sm'>
-            <div className='flex items-center relative text-black m-2.5'>
-                <a href={`/profil/${user?.id}`} className="flex items-center relative gap-2 text-black ">
-                    
-                    <img 
-                        src={user?.profile_image ? `${serverURL}${user.profile_image}` : placeholder} 
-                        alt="profile_picture" 
-                        className='rounded-full w-7 aspect-square object-cover' 
-                    />
-                    
-                 
-                    <p className='text-black w-20 overflow-wrap-anywhere whitespace-normal text-xs font-bold leading-tight'>
-                        {user ? user.name : "Chargement..."} 
-                    </p>
-                </a>
-            </div>
-            
-            <div className='flex gap-2 m-2.5'>
-                <a href="#"><img src={add} alt="add" className="h-6"/></a>
-                <a href="#"><img src={like} alt="like" className="h-6"/></a>
-            </div>
-        </div>
-    );
+      <div className="flex gap-2 m-2.5">
+        <a href="#">
+          <img src={add} alt="add" className="h-6" />
+        </a>
+        <a href="#">
+          <img src={like} alt="like" className="h-6" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default PostBar;
